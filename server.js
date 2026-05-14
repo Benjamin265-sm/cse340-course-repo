e75e5709e4d1f9cfedaf0d6f6a2a0df111f84643
Benjamin265-sm <benjaminmbaisa@gmail.com>
2026-05-14 23:37:07 +0200

    repo

--- server.js
+++ server.js
@@ -93,12 +93,6 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title });
 });
 
-////
-
-app.get('/', (req, res) => {
-  res.send('Hello from Express!');
-});
-
 // app.listen(PORT, () => {
 //   console.log(`Server is running at http://127.0.0.1:${PORT}`);
 //   console.log(`Environment: ${NODE_ENV}`);
