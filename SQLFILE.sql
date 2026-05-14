89000f647d65915491fce4d159d867bde22c8bb7
Benjamin265-sm <benjaminmbaisa@gmail.com>
2026-05-14 23:30:59 +0200

    repo

--- SQLFILE.sql
+++ SQLFILE.sql
@@ -3,7 +3,7 @@
 -- =====================================
 
 CREATE TABLE organisation (
-    organization_id SERIAL PRIMARY KEY,
+    organisation_id SERIAL PRIMARY KEY,
     name VARCHAR(150) NOT NULL,
     description TEXT NOT NULL,
     contact_email VARCHAR(255) NOT NULL,
--- src/models/organisations.js
+++ src/models/organisations.js
@@ -3,7 +3,7 @@ import db from './db.js'
 const getAllOrganisations = async() => {
     const query = `
         SELECT organisation_id, name, description, contact_email, logo_filename
-      FROM public.organisations;
+      FROM public.organisation;
     `;
 
     const result = await db.query(query);
